a02fdf3095c8f047a3f594d2f769ba43fd839fa1
muhamad aji <103230869+muhamadaji17@users.noreply.github.com>
2023-05-03 08:52:08 +0700

    Add files via upload

--- backend/day 1/BE/route/route.js
+++ backend/day 1/BE/route/route.js
@@ -1,19 +1,34 @@
-import {Router} from "express"
+import multer from "multer"
+import { Router } from "express"
 import userController from "../controller/userController.js"
+import productController from "../controller/productController.js"
 
 const route = Router()
 
 // route.get("/user", (req,res) => {
 //     res.send ("User")
 // })
-route.get("/", (req,res) => {
-    res.send ("index")
+route.get("/", (req, res) => {
+    res.send("index")
 })
+
+
+// route.post("/product", productController.createProduct)
+
 
 route.get("/user", userController.GetUsers)
 route.get("/user/:id", userController.DetailUser)
 route.post("/user", userController.CreateUser)
 route.delete("/user/:id", userController.DeleteUser)
 route.patch("/user/:id", userController.UpdateUser)
+
+route.post("/member", userController.CreateUserandCust)
+
+// route.get("/product", productController.findProduct)
+route.post("/coba", productController.createProduct)
+
+// route.get("/productCategory", productController.findProductCategory)
+// route.post("/productCategory", productController.createProductCategory)
+
 
 export default route
